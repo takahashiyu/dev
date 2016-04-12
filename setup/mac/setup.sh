498ad797697957336b11536ef220d3ecbf206991
高橋雄太 <yuta@MacBookForLearning.local>
2016-04-12 21:51:57 +0900

    セットアップ用のシェルをコミット

---
+++ setup/mac/setup.sh
@@ -0,0 +1,8 @@
+# zshellをデフォルトに設定
+chsh -s /bin/zsh
+
+# zshell の設定
+wget https://gist.githubusercontent.com/mollifier/4979906/raw/43d1c77344dd59fa119ca5b75e7a54e01e668710/zshrc_useful.sh
+mv zshrc_useful.sh .zshrc
+sourc .zshrc
+
